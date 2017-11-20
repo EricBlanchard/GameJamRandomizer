b3a7cee2004c383ce951114df15df2d9c17b9ff3
Eric Blanchard <Eric.x2@gmail.com>
2017-11-19 23:26:33 -0500

    Add a class for each option, start building out data structures.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public List<GameOption> gameOptions = new List<GameOption>();
+
+    void GenerateNewGame()
+    {
+        for (int i = 0; i < gameOptions.Count; i++)
+        {
+            if (gameOptions[i].canReroll)
+            {
+                int temp = Random.Range(0, gameOptions[i].options.Count);
+                gameOptions[i].result = gameOptions[i].options[temp];
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GameOption.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOption : MonoBehaviour {
+
+    public List<string> options = new List<string>();
+    public Text TXTlabel;
+    public Text TXTresult;
+    public bool canReroll = true;
+    public string result = "";
+}
