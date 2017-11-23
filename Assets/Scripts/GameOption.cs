bda8bdeb8558ade1b66ab14aa5d0ef6bebc1f356
Eric Blanchard <Eric.x2@gmail.com>
2017-11-22 23:50:18 -0500

    Add checkboxes for locking options and a reroll button

--- Assets/Scripts/GameOption.cs
+++ Assets/Scripts/GameOption.cs
@@ -9,6 +9,7 @@ public class GameOption : MonoBehaviour {
     public List<string> options = new List<string>();
     public TextMeshProUGUI TXTlabel;
     public TextMeshProUGUI TXTresult;
+    public Toggle toggle;
     public bool canReroll = true;
     public string result = "";
 }
